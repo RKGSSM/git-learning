3f986670b602e810c864026f3fb7e40fd085a0dd
Rahul Kumar Gupat <rahulkumarguptassm12345@gmail.com>
2026-05-26 22:10:49 +0530

    newbranch

--- first.js
+++ first.js
@@ -17,3 +17,15 @@ function payfeature(){
     console.log("pay feature added");
 }
 payfeature();
+console.log("hello world");
+function add(a,b){
+    return a+b;
+}
+add(5,10);
+console.log("sum is "+add(5,10));
+function feauture(){
+    console.log("this is a feauture function");
+}feauture();
+function newfeature(){
+    console.log("this is a new feature");
+}newfeature();
